48f5de156fb666eab9cd8c835c6142e0d67d377e
Thibaut Wybrecht <twybrech@IUT.BX1>
2017-02-01 09:47:20 +0100

    depart jasmine

---
+++ spec/GridSpec.js
@@ -0,0 +1,18 @@
+describe("Grid", function() {
+  // var player;
+  // var song;
+
+  beforeEach(function() {
+    // player = new Player();
+    // song = new Song();
+  });
+
+  it("should not be empty", function() {
+    // player.play(song);
+    // expect(player.currentlyPlayingSong).toEqual(song);
+    //
+    // //demonstrates use of custom matcher
+    // expect(player).toBePlaying(song);
+  });
+
+});
