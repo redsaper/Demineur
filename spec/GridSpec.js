94d64da1f2fb2001f304716e7df93984563091a0
Thibaut Wybrecht <twybrech@IUT.BX1>
2017-02-01 14:12:17 +0100

    merge

--- spec/GridSpec.js
+++ spec/GridSpec.js
@@ -22,8 +22,28 @@ describe("Grid", function() {
 
 describe("Case", function() {
 
-  it("should not be empty", function() {
+  var case1 = new Case(0,0,0);
+
+  it("should exist", function() {
 
     expect(Case).toBeDefined();
+
   });
+  it("case is not bomb", function() {
+
+    expect(case1.isBomb()).toBe(false);
+
+  });
+  it("case is bomb", function() {
+
+    case1.value='B';
+    expect(case1.isBomb()).toBe(true);
+  });
+  it("case has not a bomb near", function() {
+
+    case1.value=0;
+    expect(case1.value).toBe(Integer);
+  });
+
+
 });
