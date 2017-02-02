bccd06a65bcebd2a5317a80aa0a223fd81accf78
Thibaut Wybrecht <twybrech@IUT.BX1>
2017-02-02 17:27:23 +0100

    ajout tests timer

--- spec/GridSpec.js
+++ spec/GridSpec.js
@@ -124,3 +124,49 @@ describe("Case", function() {
 
 
 });
+
+describe("Timer", function() {
+
+  var timer = null;
+
+  beforeEach(function() {
+    timer = new Timer();
+  });
+
+  it("test isStarted", function() {
+
+    expect(timer.isStarted()).toBe(false);
+  });
+
+  it("test start", function() {
+
+    timer.start();
+
+    expect(timer.isStarted()).toBe(true);
+  });
+
+  it("test stop", function() {
+
+    expect(timer.isStopped()).toBe(false);
+
+    timer.start();
+    timer.stop();
+
+    expect(timer.isStopped()).toBe(true);
+  });
+
+  it("test reset", function() {
+
+    expect(timer.isReseted()).toBe(true);
+
+    timer.start();
+
+    expect(timer.isReseted()).toBe(false);
+
+    timer.reset();
+
+    expect(timer.isReseted()).toBe(true);
+    expect(timer.get()).toEqual(0);
+  });
+
+});
